e302fbea1c64e44b57819e06721b70461d3461a2
Olajide Samuel <iamosharry@gmail.com>
2024-05-16 09:17:26 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// function scrollSection(about,popular){
+  
+//   const popular = document.getElementById('popular-food')
+
+//   about.scrollIntoView({behavior: 'smooth'},)
+//   popular.scrollIntoView({behavior: 'smooth'},)
+// }
+
+const about = document.getElementById('about-us')
+
+// about.addEventListener('click',()=>{
+//   console.log('wpor');
+// })
