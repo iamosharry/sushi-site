0a1a2d3c164cb9fce1a51f23eeb0caf9d4bedce9
web3-dar <Onlineagentalmashell@gmail.com>
2025-01-20 16:00:11 +0100

    improvement 2

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {
+  animate,
+  scroll,
+} from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
 
-// Register the plugin
-gsap.registerPlugin(ScrollTrigger);
+animate(
+  ".box",
+  { scale: [0.4, 1], opacity: [0, 1] },
+  { ease: "circInOut", duration: 1 }
+);
 
-// Create the scroll-triggered animation
-gsap.to(".box", {
-  scrollTrigger: {
-    trigger: ".box",
-    start: "top center",
-    end: "bottom 100px",
-    markers: true,
-    scrub: true,
-  },
-  x: 500,
-  rotation: 360,
-  duration: 3,
-});
+animate(
+  ".feel",
+  { scale: [0.4, 1], opacity: [0, 1] },
+
+  { ease: "circInOut", duration: 1, delay: 0.5 }
+);
+
+animate(
+  ".btn1",
+  { y: [100, 0], opacity: [0, 1] },
+
+  { ease: "circInOut", duration: 1, delay: 0.8 }
+);
+
+animate(
+  ".btn2",
+  { y: [100, 0], opacity: [0, 1] },
+
+  { ease: "circInOut", duration: 1, delay: 1 }
+);
